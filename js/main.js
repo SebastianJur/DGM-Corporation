dd79e4e747361dda1c081bbfbd329645d2765bac
Sebastian Jurewicz <senkartan@gmail.com>
2023-06-09 21:12:12 +0200

    Section gallery, contact and fix mobile navigation

--- js/main.js
+++ js/main.js
@@ -1,14 +1,26 @@
 const navMobile = document.querySelector('.nav-mobile');
 const navBtn = document.querySelector('.hamburger');
 const footerYear = document.querySelector('.footer_year');
+const allNavItems = document.querySelectorAll('.nav__link');
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active');
 	navMobile.classList.toggle('nav-mobile--active');
+
+	if (navMobile.classList.contains('nav-mobile--active')) {
+		document.body.style.overflow = 'hidden';
+	} else {
+		document.body.style.overflow = 'auto';
+	}
 };
 
 const handleCurrentYear = () =>
 	(footerYear.innerText = new Date().getFullYear());
 
 navBtn.addEventListener('click', handleNav);
+navMobile.addEventListener('click', (event) => {
+	if (event.target.classList.contains('nav__link')) {
+		handleNav();
+	}
+});
 handleCurrentYear();
